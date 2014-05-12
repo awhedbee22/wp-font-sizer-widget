06623f2650f08f939c65861a5fa3ce952cd659c8
Alex Whedbee <awhedbee22@gmail.com>
2014-05-12 10:19:51 -0700

    Update Font Styles
    made smaller and more compact.

--- view.php
+++ view.php
@@ -1,5 +1,5 @@
 <h4>Font Size</h4>
 <p class="wp_widget_plugin_textarea">
-    <button class="tiny button" onclick="resizeText(-1)">-</button>
-    <button class="tiny button" onclick="resizeText(1)">+</button>
+    <button class="extrasmall button" onclick="resizeText(-1)">-</button>
+    <button class="extrasmall button" onclick="resizeText(1)">+</button>
 </p>
